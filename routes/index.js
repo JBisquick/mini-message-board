fa193da82581f698135e88173eedc103389befea
JBisquick <felipebirch@gmail.com>
2024-04-23 14:45:06 -0700

    Add form to add new messages

--- routes/index.js
+++ routes/index.js
@@ -21,12 +21,21 @@ const messages = [
   }
 ];
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', {});
+});
+
+router.post('/new', function(req, res) {
+  messages.push({text: req.body.message, user: req.body.user, added: new Date()});
+  res.redirect('/');
 });
 
 module.exports = router;
