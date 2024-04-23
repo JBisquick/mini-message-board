cb7007494c012255b66f4e69f934e29e5e63b927
JBisquick <felipebirch@gmail.com>
2024-04-23 13:35:06 -0700

    Add messages for message board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  },
+  {
+    text: "Pass the beans!",
+    user: "Bob",
+    added: new Date()
+  },
+  {
+    text: "COME ON YOU BEES!",
+    user: "Brentfordfan69",
+    added: new Date()
+  }
+];
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
 module.exports = router;
